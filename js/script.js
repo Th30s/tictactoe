f81a07ceb10438687163c5de8c3678f31c8203ce
Th30s <yahir.garcia@uasdc.org>
2020-02-03 23:09:49 +0000

    nothing

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let turnsTaken = 0;
 let gameEnd = false;
 let positionsOfX=[];
 let positionsOfO=[];
-let winningPositions = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+let winningPositions = [[[1,2,3],[4,5,6],[7,8,9]],[[1,4,7],[2,5,8],[3,6,9]],[[1,5,9],[3,5,7]]];
 
 function changePlayer(){
     if (playersTurn === "X"){
